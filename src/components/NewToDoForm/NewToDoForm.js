91c910b5a1eac4ac996b1b19cfdd32b836e25d28
maciekkociela <kocielam@gmail.com>
2019-05-09 23:59:48 +0200

    Text change

--- src/components/NewToDoForm/NewToDoForm.js
+++ src/components/NewToDoForm/NewToDoForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NewToDoForm = (props) => (
     <div>
       <input type="text" onChange={props.onChange} value={props.draft} />
-      <button onClick={props.onSubmit}>Add ToDo</button>
+      <button onClick={props.onSubmit}>Add Todo</button>
     </div>
 )
 
