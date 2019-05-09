20427b15ccd1cac20e1c42fd6c0b0a8a0c10c869
maciekkociela <kocielam@gmail.com>
2019-05-09 23:50:31 +0200

    url test

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,4 +1,4 @@
-const serveUrl = `https://cors-anywhere.herokuapp.com/https://stanwkolejceapi.herokuapp.com/api`
+const serveUrl = `https://stanwkolejceapi.herokuapp.com/api`
 
 export const toDoItemsApiUrl = id =>
     id ? `${serveUrl}/todos/${id}` : `${serveUrl}/todos/` 
